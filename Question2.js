01a1ad4c1c66dbb0d1acc1f7b02d1020e21bb28d
Ali Hassan <alihassan.education@gmail.com>
2023-04-17 22:13:47 +0500

    first

--- Question2.js
+++ Question2.js
@@ -1,10 +1,10 @@
-let number = prompt("Enter a number:");
-number = parseInt(number);
+// let number = prompt("Enter a number:");
+// number = parseInt(number);
 
-if (number > 0) {
-  alert("The Sign is (+)");
-} else if (number < 0) {
-  alert("The Sign is (-)");
-} else {
-  alert("The number is zero (0)");
-}
+// if (number > 0) {
+//   alert("The Sign is (+)");
+// } else if (number < 0) {
+//   alert("The Sign is (-)");
+// } else {
+//   alert("The number is zero (0)");
+// }
